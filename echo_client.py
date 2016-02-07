0c68fcac73fb1d72f8db7b7b278b8c5649744749
eliotbush <bush.eliot@gmail.com>
2016-02-06 22:39:51 -0500

    Update echo_client.py

--- echo_client.py
+++ echo_client.py
@@ -1,21 +1,5 @@
 import socket
 import sys
-import time
-import threading
-
-def send_message(s):
-    #global s
-    while True:
-        #send message
-        message = input()
-        s.send(message.encode('ascii'))
-        
-def get_update(s):
-    while True:
-        s.send("UPDATE!".encode('ascii'))
-        time.sleep(5)
-    
-    
 
 #server address/port
 server_address = ('10.0.0.69', 12345)
@@ -27,12 +11,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print ('connecting to ' + str(server_address))
 s.connect(server_address)
 print ('username:')
-t_prev = time.clock()
-while True:      
-    getInput = threading.Thread(name='input', target=send_message(s))
-    updateText = threading.Thread(name='update', target=get_update)
-    getInput.start()
-    updateText.start()
+
+while True:
+    message = input()
+    
+    #send message
+    #print("sending " + message)
+    s.send(message.encode('ascii'))
 
     #read response
     data = s.recv(1024).decode('ascii')
